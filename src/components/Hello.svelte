12ebf2de339f9c8c730b0521b68e26cd09f42b3f
aiotrope <arneli74@protonmail.com>
2023-09-05 11:54:44 +0300

    🐛 fix(app): solutions for hello astro and svelte

---
+++ src/components/Hello.svelte
@@ -0,0 +1,17 @@
+<script>
+  const message = 'Svelte';
+
+  let isSvelte = true;
+
+  const updateText = () => {
+    isSvelte = !isSvelte;
+  };
+</script>
+
+{#if isSvelte}
+  <h1>Hello {message}!</h1>
+{:else}
+  <h1>Hello Astro!</h1>
+{/if} 
+
+<button on:click={updateText}>Update!</button>
